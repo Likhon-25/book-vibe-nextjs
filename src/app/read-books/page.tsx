a949ae5b6f54c600c6013860110de1080be9af7d
Likhon-25 <likhonmondol327@gmail.com>
2026-09-21 02:45:48 +0600

    finally succes to add chart

--- src/app/read-books/page.tsx
+++ src/app/read-books/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import React from "react";
 import {
@@ -14,7 +14,22 @@ import {
   Label,
 } from "recharts";
 
-const colors = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', 'red', 'pink', 'black'];
+const colors = [
+  "#0088FE",
+  "#00C49F",
+  "#FFBB28",
+  "#FF8042",
+  "red",
+  "pink",
+  "black",
+];
+
+const getPath = (x: number, y: number, width: number, height: number) => {
+  return `M${x},${y + height}C${x + width / 3},${y + height} ${x + width / 2},${y + height / 3}
+  ${x + width / 2}, ${y}
+  C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${x + width}, ${y + height}
+  Z`;
+};
 
 const TriangleBar = (props: BarShapeProps) => {
   const { x, y, width, height, index } = props;
@@ -28,7 +43,7 @@ const TriangleBar = (props: BarShapeProps) => {
       stroke={color}
       fill={color}
       style={{
-        transition: 'stroke-width 0.3s ease-out',
+        transition: "stroke-width 0.3s ease-out",
       }}
     />
   );
